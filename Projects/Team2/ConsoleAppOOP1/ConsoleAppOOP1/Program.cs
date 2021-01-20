c890312b82c90a626130ed8b365174033fd011d6
Roman-Derback <derback2018@gmail.com>
2021-01-20 16:42:03 +0200

    Перевантажені оператори
    Task 1,2

---
+++ Projects/Team2/ConsoleAppOOP1/ConsoleAppOOP1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleAppOOP1
+{
+    class Program
+    {
+        public class Person
+        {
+            int count;
+            string sexinfo;
+            public string name = "Віталій";
+            public int age = 18;
+            public int sex = 1;
+            public long telephonenumber = 0968484332;
+
+            public void PersonPrint()
+            {
+                if (sex == 1)
+                {
+                    sexinfo = "Чоловік";
+                }
+                if (sex == 2)
+                {
+                    sexinfo = "Жінка";
+                }
+                else if(sex!=2&&sex!=1)
+                    sexinfo = "Стать не визначена";
+                Console.WriteLine("Iм'я: "+name+"\nВiк:"+ age + "\nСтать:" + sexinfo + "\nТелефон: " +telephonenumber);
+                Console.ReadKey();
+            }
+            public string Name()
+            {
+                Console.WriteLine("Введіть ім'я: ");
+                name = Console.ReadLine();
+                return name;
+            }
+            public string Age()
+            {
+                Console.WriteLine("Введіть вік: ");
+                age = Convert.ToInt32(Console.ReadLine());
+                return age.ToString();
+            }
+            public string Sex()
+            {
+                Console.WriteLine("Введіть стать 1 = чоловік/2 = жінка: ");
+                sex = Convert.ToInt32(Console.ReadLine());
+                return sex.ToString();
+            }
+            public string Telephone()
+            {
+                Console.WriteLine("Введіть телефон: ");
+                telephonenumber = Convert.ToInt64(Console.ReadLine());
+                return telephonenumber.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Person vetal = new Person();
+            vetal.PersonPrint();
+            Povtor:
+            Console.WriteLine("Введіть операцію: 1 змінити ім'я / 2 змінити кількість років / 3 змінити стать / 4 змінити телефон");
+            count = Convert.ToInt32(Console.ReadLine());
+            if (count == 1)
+            {
+                vetal.Name();
+                vetal.PersonPrint();
+                goto Povtor;
+            }
+            if(count == 2)
+            {
+                vetal.Age();
+                vetal.PersonPrint();
+                goto Povtor;
+            }
+            if (count == 3)
+            {
+                vetal.Sex();
+                vetal.PersonPrint();
+                goto Povtor;
+            }
+            if (count == 4)
+            {
+                vetal.Telephone();
+                vetal.PersonPrint();
+                goto Povtor;
+            }
+            if(count <1||count>4)
+            {
+                Console.WriteLine("Операція відсутня");
+                vetal.PersonPrint();
+                goto Povtor;
+            }          
+        }
+    }
+}
---
+++ Projects/Team2/ConsoleAppOOP2/ConsoleAppOOP2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppOOP2
+{
+    class Program
+    {
+        class _String
+        {
+            string userInput = Console.ReadLine();
+            public _String()///конструктор 1
+            {
+                if (userInput.Length <= 80&& userInput.Length != 0)
+                {
+                    Console.Write("Довжина в межаш діапазону\n"+ userInput.ToString());
+                }
+                else if (userInput.Length > 80)
+                {
+                    Console.Write("Довжина більша ніж 80 символів");
+                }
+                else if(userInput.Length == 0)
+                    Console.Write("Рядок порожній");
+            }
+
+            public _String(int lan)///конструктор 2
+            {
+                int bufSize = lan;
+                Stream inStream = Console.OpenStandardInput(bufSize);
+                Console.SetIn(new StreamReader(inStream, Console.InputEncoding, false, bufSize));
+                if (userInput.Length <= bufSize && userInput.Length != 0)
+                {
+                    Console.WriteLine("символів ="+userInput?.Length);
+                }
+                else if (userInput.Length > bufSize)
+                {
+                    Console.WriteLine("Довжина більша ніж "+lan+" символів");
+                }
+                else if (userInput.Length == 0)
+                    Console.WriteLine("Рядок порожній");
+            }
+
+            public _String(int lan, string standartword)///конструктор 3
+            {
+                int bufSize = lan;
+                string sr1 = standartword;
+                Console.WriteLine("Words: " + standartword+ standartword?.Length);
+                Stream inStream = Console.OpenStandardInput(bufSize);
+                Console.SetIn(new StreamReader(inStream, Console.InputEncoding, false, bufSize));
+                if (userInput.Length <= bufSize && userInput.Length != 0)
+                {
+                    Console.WriteLine("символів = " + standartword?.Length);
+                }
+                else if (userInput.Length > bufSize)
+                {
+                    Console.WriteLine("Довжина більша ніж " + lan + " символів");
+                }
+                else if (userInput.Length == 0)
+                    Console.WriteLine("Рядок порожній");
+            }
+        }
+        static void Main(string[] args)///Main
+        {
+            int c;
+            string sdtr = "Рядок за замовчуванням ", a;
+            Console.WriteLine("Конструктор 1!");
+            _String sd = new _String();
+            Console.WriteLine("\nКонструктор 2!");
+            _String sg = new _String(1024);
+            Console.WriteLine("Конструктор 3!");
+            _String sс = new _String(1024,sdtr);
+            Console.WriteLine("Бажаєте змінити рядок?  1 = так / 2 = ні");
+            try
+            {
+                c = Convert.ToInt32(Console.ReadLine());
+            }catch
+            {
+                c = 2;
+            }
+            switch(c)
+            {
+                case 1:
+                    Console.WriteLine("Введіть новий рядок: ");
+                    a = Console.ReadLine();
+                    sdtr = a;
+                    Console.WriteLine("Новий рядок " + sdtr + " розмір " + sdtr?.Length);
+                    break;
+                case 2:
+                    return;
+                    break;
+                default: return;
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
