377955b0df63bbde7f79d72ccb706c3ab75c0d40
Dniluk111 <77339748+Dniluk111@users.noreply.github.com>
2021-01-20 17:42:08 +0200

    дз

---
+++ Projects/Team2/Курси, Масиви задача №1/Курси, Масиви задача №1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курси__Масиви_задача__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masiv = new int[6] { 1, 66, 900, 11, 75, 33 };
+            for (int i = masiv.Length - 1; i >= 0; --i)
+                Console.Write(masiv[i] + " , ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/Team2/Курси, Масиви, завдання №2/Курси, Масиви, завдання №2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курси__Масиви__завдання__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Введiть кiлькiсть елементiв масиву: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введiть {0}-й элемент", i + 1);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    Console.Write("Парне : (" + a[i] + ")");
+                }
+                else if(a[i]%2!=0)
+                {
+                    Console.Write("Не парне : (" + a[i] + ")");
+                }
+                Console.Write(a[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
