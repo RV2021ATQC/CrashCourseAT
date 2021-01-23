beba29bc422a6e8205f95f1586be72766460b5ba
Roman-Derback <derback2018@gmail.com>
2021-01-24 00:15:01 +0200

    New version of program
    and bug report

---
+++ Projects/Team2/derback top secret/Program №1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleAppOOP
+{
+    class Program
+    {
+        public class Person
+        {
+            string sexinfo;
+            public string name;
+            public int age;
+            public int sex;
+            public int telephonenumber;
+
+            public Person(string a, int b, int c, long d)
+            {
+                name = "Nameless"; age = 18; sex = 1; telephonenumber = 0000000000;
+            }
+
+            public void PersonPrint(string name, int age, int sex, long telephonenumber)
+            {
+                switch(sex)
+                {
+                    case 1:
+                        sexinfo = "Man";
+                        break;
+                    case 2:
+                        sexinfo = "Woman";
+                        break;
+                    default: sexinfo = "Sex unassigned";
+                        break;
+                }
+                Console.WriteLine($"Name: {name}\nAge: {age}\nSex: {sexinfo}\nTelephone: {telephonenumber}");
+                Console.ReadKey();               
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string a;
+            int b, c;
+            long d;
+            Console.WriteLine("Enter name: ");
+            a = Console.ReadLine();
+            Console.WriteLine("Enter age: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter sex 1 = man/2 = woman: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter telephone: ");
+            d = Convert.ToInt64(Console.ReadLine());
+            Person jack = new Person(a,b,c,d);          
+            jack.PersonPrint(a, b, c, d);
+        }
+    }
+}
---
+++ Projects/Team2/derback top secret/Program №2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ConsoleAppOOP1
+{
+    class Program
+    {
+        public class Person
+        {
+            int count;
+            string sexinfo;
+            public string name = "Vetal";
+            public int age = 18;
+            public int sex = 1;
+            public long telephonenumber = 0968484332;
+
+            public void PersonPrint()
+            {
+                switch (sex)
+                {
+                    case 1:
+                        sexinfo = "Man";
+                        break;
+                    case 2:
+                        sexinfo = "Woman";
+                        break;
+                    default:
+                        sexinfo = "Sex unassigned";
+                        break;
+                }
+                Console.WriteLine($"Name: {name}\nAge: {age}\nSex: {sexinfo}\nTelephone: {telephonenumber}");
+                Console.ReadKey();
+            }
+            public string Name()
+            {
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                return name;
+            }
+            public string Age()
+            {
+                Console.WriteLine("Enter age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+                return age.ToString();
+            }
+            public string Sex()
+            {
+                Console.WriteLine("Enter sex 1 = man/2 = woman: ");
+                sex = Convert.ToInt32(Console.ReadLine());
+                return sex.ToString();
+            }
+            public string Telephone()
+            {
+                Console.WriteLine("Enter telephone: ");
+                telephonenumber = Convert.ToInt64(Console.ReadLine());
+                return telephonenumber.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Person vetal = new Person();
+            vetal.PersonPrint();
+            Povtor:
+            Console.WriteLine("Select operation: 1 change name / 2 change age / 3 change sex / 4 change telephone");
+            count = Convert.ToInt32(Console.ReadLine());
+            switch(count)
+            {
+                case 1:
+                    vetal.Name();
+                    vetal.PersonPrint();
+                    goto Povtor;
+                case 2:
+                    vetal.Age();
+                    vetal.PersonPrint();
+                    goto Povtor;
+                case 3:
+                    vetal.Sex();
+                    vetal.PersonPrint();
+                    goto Povtor;
+                case 4:
+                    vetal.Telephone();
+                    vetal.PersonPrint();
+                    goto Povtor;
+                default:
+                    Console.WriteLine("Unknown operation");
+                    vetal.PersonPrint();
+                    goto Povtor;
+            }
+        }
+    }
+}
