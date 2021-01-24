923b91b69c6c74143860f44785159bbbf614025b
Dniluk111 <77339748+Dniluk111@users.noreply.github.com>
2021-01-24 22:37:49 +0200

    Arrays

---
+++ Projects/Team2/Arrays, Task 2/Arrays, Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrays__Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Введiть кiлькiсть елементiв масиву: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введiть {0}-й элемент", i + 1);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    Console.Write("Парне : (" + a[i] + ")");
+                }
+                else if (a[i] % 2 != 0)
+                {
+                    Console.Write("Не парне : (" + a[i] + ")");
+                }
+                Console.Write(a[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
