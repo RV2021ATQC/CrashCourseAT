7dcadca68602727c4dc6f7e237aec1897da83742
alexfrstnberg <55796418+alexfrstnberg@users.noreply.github.com>
2021-01-24 22:30:21 +0200

    Update Converter.cs

--- Projects/Team1/Prystopchuk/Converter.cs
+++ Projects/Team1/Prystopchuk/Converter.cs
@@ -2,9 +2,9 @@
  * from decimal to other base-N systems (N<=36)
  * Return type: string*/
 
-namespace TEMP
+namespace Converter
 {
-    class Converter
+    class Program
     {
         
         static string convert(int num, int base_, string res = "")
@@ -21,9 +21,7 @@ namespace TEMP
             {
                 System.Console.WriteLine(e.Message);
                 return null;
-
             }
-
         }
 
         static void Main()
@@ -39,31 +37,8 @@ namespace TEMP
                 {
                     System.Console.SetCursorPosition(x, ++y);
                     System.Console.WriteLine($"{num} --- {convert(num, base_)}");
-                }
-                //System.Console.SetCursorPosition(x+=10, 0);                
+                }             
             }
-
         }
-
-
-        //static string ToHex(int value)
-        //{
-        //    return System.String.Format("{0:X}", value);
-        //}
-        //static string Reverse(string str)
-        //{
-        //    char[] stringArray = str.ToCharArray();
-        //    System.Array.Reverse(stringArray);
-        //    return new string(stringArray);
-        //}
-
-        //static string convert(int num, int base_, string res = "")
-        //{
-        //    if (num == 0)
-        //        return Reverse(res);
-        //    int dig = num % base_;
-        //    res += dig.ToString();
-        //    return convert(num / base_, base_, res);
-        //}
     }
 }
