59ebbfb0089718b0b03ca668f2df7328635d4c1f
Anton <romanukhaav@gmail.com>
2021-01-26 11:36:23 +0200

    add unitTest

--- Projects/Romanukha/SimplePrograms/Lesson2/Plants.cs
+++ Projects/Romanukha/SimplePrograms/Lesson2/Plants.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-//using NUnit.Framework;
+using NUnit.Framework;
 
 namespace Lesson2
 {
-    public class Plants : ICook
+    //для вищих рівнів ієрархії використовують абстрактні класи
+    //дані класи можуть місти неабстракті методи
+    //можливо один абстрактний клас наслідувати від іншого
+
+    /*abstract*/ public class Plants : ICook
     {
+        //якщо ми хочемо ініціалізувати об'єкт класу нащадка, то
+        //ми мусимо перевизначити УСІ абстрактні методи. 
+        //Неможливо створити об'єкт, якщо в класі ще існують абстракції
+        //   abstract string getPageNum();
         public int Age
         { get; set; }
 
         public int height;
         public bool isEdible;
 
-     //   abstract string getPageNum();
+
 
         public Food GetFood()
         {
@@ -27,21 +35,20 @@ namespace Lesson2
         }
     }
 
-
-    //[TestFixture]
-    //public class TestPlants
-    //{
-    //    [Test]
-    //    public void TestSum()
-    //    {
-    //        Assert.Pass();
-    //        var plant = new Plants();
-    //        int ExpectedValue = 5;
-    //        // Assert.IsTrue(plant.Sum(2, 3) == 5);
-    //        //  Assert.Equals(plant.Sum(2, 3), ExpectedValue);
-    //        //Assert.IsFalse(ExpectedValue.ToString() == "1 should not be prime");
-    //        // Assert.IsFalse(ExpectedValue.ToString() != "1 should not be prime");
-    //    }
-    //}
+    [TestFixture]
+    public class TestPlants
+    {
+        [Test]
+        public void TestSum()
+        {
+            Assert.Pass();
+            var plant = new Plants();
+            int ExpectedValue = 5;
+            Assert.IsTrue(plant.Sum(2, 3) == 5);
+            Assert.Equals(plant.Sum(2, 3), ExpectedValue);
+            Assert.IsFalse(ExpectedValue.ToString() == "1 should not be prime");
+            Assert.IsFalse(ExpectedValue.ToString() != "1 should not be prime");
+        }
+    }
 }
 
