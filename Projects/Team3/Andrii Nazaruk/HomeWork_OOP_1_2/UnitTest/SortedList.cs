a3ec678b6501349bb96f9bbb785e8fe7747f77b8
Chadob0y <nazar-90@ukr.net>
2021-01-27 18:30:08 +0200

    Add unit test
    Add unit test

---
+++ Projects/Team3/Andrii Nazaruk/HomeWork_OOP_1_2/UnitTest/SortedList.cs
@@ -0,0 +1,36 @@
+﻿using HomeWork_OOP_1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace UnitTest
+{
+    public class SortedList
+    {
+        [Fact]
+        public void SortListByModel()
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "BMW", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "Audi", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "Ford", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+
+            var result = cars.OrderBy(x => x.Model);
+
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void SortListByMark()
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "BMW", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "Audi", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+            cars.Add(new Car { IsWorking = true, TransportType = "Car", Mark = "X5", Model = "Ford", Price = 25.3, Speed = 100, YearOfExploatation = 15 });
+
+            var result = cars.OrderBy(x => x.Model);
+            Assert.NotNull(result);
+        }
+    }
+}
