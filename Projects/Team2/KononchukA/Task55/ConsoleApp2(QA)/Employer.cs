ae7ae20e3a99baf2ba0c804484d82a583748eb62
cookikus <kononchuk_ak19@nuwm.edu.ua>
2021-01-28 16:04:46 +0200

    add task44 task55 OOP
    add task44 task55 OOP

---
+++ Projects/Team2/KononchukA/Task55/ConsoleApp2(QA)/Employer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2_QA_
+{
+    public abstract class Employer
+    {
+        public string name;
+        public int age;
+        public Employer(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+        }
+    }
+}
---
+++ Projects/Team2/KononchukA/Task55/ConsoleApp2(QA)/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2_QA_
+{
+    public class Manager:Employer
+    {
+        public Manager(string name, int age) : base(name, age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Manager information");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine("");
+        }
+    }
+}
