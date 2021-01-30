9adfd4b0eaeb261bea81534aacbcbda0dda8f52d
Overlord <den25051999@gmail.com>
2021-01-30 19:47:54 +0200

    Commit

--- Projects/Team1/Semeniuk/Feature_with_project_class_Animal_Fish/StarProject/TestAnimals.cs
+++ Projects/Team1/Semeniuk/Feature_with_project_class_Animal_Fish/StarProject/TestAnimals.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StarProject
 {
-    class TestAnimals
+    [TestFixture]
+    public class TestAnimals
     {
+        [Test]
+        //[Category("Animals")]
+        public void TestGetAge()
+        {
+            //Given
+            double year = 2013.5;
+            var animal = new Animals(year, "red");
+            int ExpectedValue = Convert.ToInt32(DateTime.Now.Year) - (int)year;
+
+            //When
+            var ActualValue = animal.GetAge();
+
+            //Then
+            Console.WriteLine("TestGetAge() execution");
+            Assert.IsTrue(ActualValue == ExpectedValue);
+        }
     }
 }
