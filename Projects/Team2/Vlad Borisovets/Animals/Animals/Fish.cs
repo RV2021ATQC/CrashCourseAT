7a1819440b59e3a46aa1de3a09d5d8a55517bb1b
VladislavBr <77299225+VladislavBr@users.noreply.github.com>
2021-01-31 20:14:38 +0200

    Add Homework

---
+++ Projects/Team2/Vlad Borisovets/Animals/Animals/Fish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Animals
+{
+	[Serializable]
+	public class Fish: Animals
+	{
+		[XmlElement()]
+		public string kind;
+		[XmlElement()]
+		public string species;
+		public Fish()
+		{
+
+		}
+		public Fish(int yearOfBirth, string color, string kind= "Mackerel", string species= "Amberjack") :base(yearOfBirth, color)
+		{
+			this.kind = kind;
+			this.species = species;
+		}
+	}
+}
---
+++ Projects/Team2/Vlad Borisovets/Animals/Animals/TestAnimals.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Animals
+{
+	class TestAnimals
+	{
+		[Test]
+		[Category("AnimalsTest")]
+		//Get age test
+		public void GetAgeTest()
+		{
+			//Given
+			int ExpectedValue = 20;
+
+			//When
+			var ActualValue = Program.GetAge(2001);
+
+			//Then
+			Assert.AreEqual(ActualValue, ExpectedValue);
+		}
+		[Test]
+		[Category("AnimalsTest")]
+		//Is the list empty
+		public void EmtyTest()
+		{
+			Assert.That(Program.animals, Is.Empty);
+		}
+		[Category("Regression")]
+		[TestCase(2001, 20)]
+		[TestCase(2005, 16)]
+		[TestCase(2003, 18)]
+		[TestCase(2008, 13)]
+		[TestCase(2011, 10)]
+		public void GetAgeTest2(int age, int expectedPrice)
+		{
+			// act
+
+			int actualAge = Program.GetAge(age);
+
+			// assert
+
+			Assert.AreEqual(expectedPrice, actualAge);
+		}
+	}
+	
+	
+}
+
