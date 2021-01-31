3c6e9d185327e0b7bf430c61cffc4e6d383acb2c
Overlord <den25051999@gmail.com>
2021-02-01 01:17:07 +0200

    Update TestProgram.cs

--- Projects/Team1/Semeniuk/Feature_with_project_class_Animal_Fish/StarProject/TestProgram.cs
+++ Projects/Team1/Semeniuk/Feature_with_project_class_Animal_Fish/StarProject/TestProgram.cs
@@ -41,13 +41,15 @@ namespace StarProject
             var field4 = "species";
             var field5 = "weight";
             var older = 2;
-            var AnimalCollection = new List<Animals>();
             //попередньо введені дані
-            AnimalCollection.Add(new Animals(1234.5, "red"));
-            AnimalCollection.Add(new Animals(2020, "blue"));
-            AnimalCollection.Add(new Animals(4321, "brown"));
-            AnimalCollection.Add(new Animals(4234, "yellow"));
-            AnimalCollection.Add(new Fish(2015, "white", "sea fish", "normal"));
+            var AnimalCollection = new List<Animals> 
+            { 
+                new Animals(1234.5, "red"), 
+                new Animals(2020, "blue") ,
+                new Animals(4321, "brown") ,
+                new Animals(4234, "yellow") ,
+                new Fish(2015, "white", "sea fish", "normal")
+            };
             var ExpectedValue = $"Now {field1} = 1234,5. obj(1)\nNow {field2} = red. obj(1)\nNow {field3} = sea fish. obj(5)\nNow {field5} = 0,1. obj(5)\nNow {field1} = 2015. obj(5)\nNow {field4} = normal. obj(5)\nNow {field2} = white. obj(5)\n";
 
             //When
