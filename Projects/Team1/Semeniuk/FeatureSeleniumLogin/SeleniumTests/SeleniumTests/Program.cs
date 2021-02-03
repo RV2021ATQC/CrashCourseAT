e7505073f4a771615ced2d1768f3ce58940f830b
Overlord <den25051999@gmail.com>
2021-02-04 00:56:17 +0200

    UpdateByGirkin

--- Projects/Team1/Semeniuk/FeatureSeleniumLogin/SeleniumTests/SeleniumTests/Program.cs
+++ Projects/Team1/Semeniuk/FeatureSeleniumLogin/SeleniumTests/SeleniumTests/Program.cs
@@ -34,25 +34,34 @@ namespace SeleniumTests
         [Test]
         public void FirstTest()
         {
-            Console.WriteLine("FirstTest1() ThreadID= " + Thread.CurrentThread.ManagedThreadId);
+            //Given
+            string email = "den25051999@gmail.com";
+            string password = "admin1234";
+            string webSite = "https://vseosvita.ua/";
+            string personalOffice = "https://vseosvita.ua/user/id1277827";
+            string buttonText = "+ Додати матеріал";
 
+            //When
+            Console.WriteLine("FirstTest1() ThreadID= " + Thread.CurrentThread.ManagedThreadId);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             //Launching test page
-            driver.Navigate().GoToUrl("https://vseosvita.ua/");
+            driver.Navigate().GoToUrl(webSite);
             //Click on drop-down menu
             driver.FindElement(By.XPath("/html/body/header/div/div/div[1]/a[2]")).Click();
             //go to login page
             driver.FindElement(By.CssSelector("body > header > div > div > div.v-hide-on-mobile.vr-hide-on-mobile-new > div > div.n-row-menu.n-row-top-menu > div.n-menu-col.n-menu-col-kabinet > div.a-registration > a:nth-child(2)")).Click();
             //e-mail
-            driver.FindElement(By.Id("login-email")).SendKeys("den25051999@gmail.com");
+            driver.FindElement(By.Id("login-email")).SendKeys(email);
             //password
-            driver.FindElement(By.Id("login-password")).SendKeys("admin1234" + Keys.Enter);
+            driver.FindElement(By.Id("login-password")).SendKeys(password + Keys.Enter);
             Thread.Sleep(2000);
             //Checking for new(after login) functions
-            driver.Navigate().GoToUrl("https://vseosvita.ua/user/id1277827");
+            driver.Navigate().GoToUrl(personalOffice);
             Thread.Sleep(2000);
             IWebElement actual = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/center/div/a"));
-            Assert.AreEqual(actual.Text, "+ Додати матеріал");
+
+            //Then
+            Assert.AreEqual(actual.Text, buttonText);
         }
     }
 }
