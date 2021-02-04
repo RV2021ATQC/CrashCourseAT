3b01fdcd09dfb3263606a8512d998413108fde11
taniamos09 <77333258+taniamos09@users.noreply.github.com>
2021-02-04 22:21:39 +0200

    TransportCollection. Version02

---
+++ Projects/Team3/Mosiychuk Tania/TransportCollection/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransportCollection
+{
+    public class Ship : Transport
+    {
+        public Ship() { }
+        public Ship(string TypeOfTransport, string Mark, long Price, int YearOfExploatation, int Speed) : base
+            (TypeOfTransport, Mark, Price, YearOfExploatation, Speed)
+        {  }
+    }
+}
+
---
+++ Projects/Team3/Mosiychuk Tania/TransportCollection/Transport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TransportCollection
+{
+    [XmlInclude(typeof(Transport))]
+    [Serializable]
+    public abstract class Transport
+    {
+        public string TypeOfTransport;
+        public string Mark;
+        public long Price;
+        public int YearOfExploatation;
+        public int Speed;
+
+        public Transport() { }
+        public Transport(string TypeOfTransport, string Mark, long Price, int YearOfExploatation, int Speed)
+        {
+            this.TypeOfTransport = TypeOfTransport;
+            this.Mark = Mark;
+            this.Price = Price;
+            this.YearOfExploatation = YearOfExploatation;
+            this.Speed = Speed;
+        }
+    }
+}
