7842fe7f13febe34b2218254c360e6ce7246f428
VladislavBr <77299225+VladislavBr@users.noreply.github.com>
2021-02-06 19:27:22 +0200

    Parallel Selenium test

---
+++ Projects/Team2/Vlad Borisovets/UnitTestProject/UnitTestProject/AuthorizationPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Interactions;
+
+namespace UnitTestProject
+{
+	public class AuthorizationPage
+	{
+		public string Login;
+		public string Password;
+		private static string OpenLoginForm = "#w1 > li:nth-child(2) > a";
+		private static string LoginFiled = "studentloginform-login";
+		private static string PasswordFiled = "studentloginform-password";
+		public static void goToSite(IWebDriver driver, string site)
+		{
+			driver.Navigate().GoToUrl(site);
+			Thread.Sleep(1000);
+		}
+		public static void Logout(IWebDriver driver, AuthorizationPage account)
+		{
+			driver.FindElement(By.CssSelector(OpenLoginForm)).Click();
+			driver.FindElement(By.Id(LoginFiled)).SendKeys(account.Login);
+			driver.FindElement(By.Id(PasswordFiled)).SendKeys(account.Password + Keys.Enter);
+			Thread.Sleep(2000);
+		}
+		public AuthorizationPage(string Login, string Password)
+		{
+			this.Login = Login;
+			this.Password = Password;
+		}
+
+
+	}
+}
---
+++ Projects/Team2/Vlad Borisovets/UnitTestProject2/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Interactions;
+
+namespace UnitTestProject
+{
+	public class UnitTest1
+	{
+		public IWebDriver driverChrome;
+		public IWebDriver driverFirefox;
+		
+		[OneTimeTearDown]
+		public void EndTest()
+		{
+			driverChrome.Quit();
+			driverFirefox.Quit();
+		}
+		
+		[Test]
+		[Parallelizable]
+		public void LoginChrome()
+		{
+			driverChrome = new ChromeDriver();
+			// prepare
+			AuthorizationPage.goToSite(driverChrome, "https://naurok.ua/");
+
+
+			// action
+			AuthorizationPage account = new AuthorizationPage("vladb707@gmail.com", "test_worked");
+			AuthorizationPage.Logout(driverChrome, account);
+
+
+			// verification      
+			string url = "https://naurok.ua/student/dashboard";
+			Assert.AreEqual(url, driverChrome.Url);
+		}
+		
+		[Test]
+		[Parallelizable]
+		public void LoginFirefox()
+		{
+			driverFirefox = new FirefoxDriver();
+			// prepare
+			AuthorizationPage.goToSite(driverFirefox, "https://naurok.ua/");
+
+
+			// action
+			AuthorizationPage account = new AuthorizationPage("vladb707@gmail.com", "test_worked");
+			AuthorizationPage.Logout(driverFirefox, account);
+
+
+			// verification      
+			string url = "https://naurok.ua/student/dashboard";
+			Assert.AreEqual(url, driverFirefox.Url);
+		}
+
+
+	}
+}
