c6be73626de82bed93adc43c81eaf0dec58c36c1
Roman-Derback <derback2018@gmail.com>
2021-02-12 12:32:39 +0200

    deleted usless fields

--- Projects/Team2/derbackAPP/SeleniumTest.cs
+++ Projects/Team2/derbackAPP/SeleniumTest.cs
@@ -28,21 +28,13 @@ namespace SeleniumTest
             driver.Navigate().GoToUrl("https://lite.pubg.com/ru/download/");
             driver.Navigate().GoToUrl("https://lite.pubg.com/ru/news/");
             Assert.AreEqual(link, driver.Url);
-            Thread.Sleep(2000);
             driver.FindElement(By.XPath("/html/body/div[2]/header/div/div[2]/aside/ul/li[1]/a")).Click();
             Thread.Sleep(1000);
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-email-input > div > input")).Click();
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-email-input > div > input")).SendKeys("derback2015@ukr.net");
-            Thread.Sleep(1000); 
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-password-input > div > input")).Click();
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-password-input > div > input")).SendKeys("GoodParol100%");
-            Thread.Sleep(1000);
             driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/form/div[2]/app-form-button/div/button")).Click();
-        }
-
-        static void Main(string[] args)
-        {
-
         }
     }
 }
