00714d1d2b443aed521560fc0d8853b90ac96831
Roman-Derback <derback2018@gmail.com>
2021-02-15 12:33:31 +0200

    add data validation

--- Projects/Team2/derbackAPP/SeleniumTest.cs
+++ Projects/Team2/derbackAPP/SeleniumTest.cs
@@ -32,9 +32,21 @@ namespace SeleniumTest
             Thread.Sleep(1000);
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-email-input > div > input")).Click();
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-email-input > div > input")).SendKeys("derback2015@ukr.net");
+            //check email
+            var emailText = driver.FindElement(By.CssSelector("#loginForm > div.column > app-email-input > div > input")).GetAttribute("value");
+            Assert.AreEqual(emailText, "derback2015@ukr.net");
+            //
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-password-input > div > input")).Click();
             driver.FindElement(By.CssSelector("#loginForm > div.column > app-password-input > div > input")).SendKeys("GoodParol100%");
+            //check password
+            var parolText = driver.FindElement(By.CssSelector("#loginForm > div.column > app-password-input > div > input")).GetAttribute("value");
+            Assert.AreEqual(parolText, "GoodParol100%");
+            //
             driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/form/div[2]/app-form-button/div/button")).Click();
+            //check how change Button (login/logout) 
+            var buttonlog = driver.FindElement(By.CssSelector("#logoutComponent > a > span")).Text;
+            Assert.AreEqual(buttonlog, "LOGOUT");
+            //
         }
     }
 }
