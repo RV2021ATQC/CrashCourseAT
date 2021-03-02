fc4dcfa1fa63034b3f50ef0a02add9af6f1934a7
Overlord <den25051999@gmail.com>
2021-03-02 12:06:16 +0200

    Start

---
+++ Projects/Team1/TestFrameworkForOpenCart/TestFramework.OpenCart/TestFramework.OpenCart/Pages/ABasePage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+
+namespace SeleniumTests
+{
+    public abstract class ABasePage
+    {
+        
+        protected readonly IWebDriver driver;
+
+        protected ABasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        abstract protected void VerifyWebElements();
+        //public void Wait()
+        //{
+        //    using (var driver = new FirefoxDriver())
+        //    {
+        //        var foo = new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+        //                        .Until(drv => drv.FindElement(By.Name("q")));
+        //        Debug.Assert(foo.Text.Equals("Hello from JavaScript!"));
+        //    }
+        //}
+    }
+}
---
+++ Projects/Team1/TestFrameworkForOpenCart/TestFramework.OpenCart/TestFramework.OpenCart/Pages/MainPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    public class MainPage : ABasePage
+    {
+        public MainPage(IWebDriver driver) : base(driver){ GoToMain(); VerifyWebElements(); }
+        public IWebElement Logo => driver.FindElement(By.XPath("//div[@id='logo']/h1/a"));
+        public void GoToMain() => driver.Navigate().GoToUrl("http://localhost/OpencartStore/index.php?route=common/home");
+        public IWebElement registrationDropdownMenu => driver.FindElement(By.XPath("//a[@class='dropdown-toggle']/span[@class='caret']"));
+        public IWebElement registrationPageButton => driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']/li[1]/a"));
+        public IWebElement loginPageButton => driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']/li[2]/a"));
+        public IWebElement logoutButton => driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']/li[5]/a"));
+        public IWebElement successfulyLogout => driver.FindElement(By.XPath("//ul[@class='breadcrumb']/li[3]/a[contains(text(), 'Logout')]"));
+        public MainPage StartRegistration()
+        {
+            registrationDropdownMenu.Click();
+            registrationPageButton.Click();
+            return this;
+        }
+        public void StartLogin()
+        {
+            registrationDropdownMenu.Click();
+            loginPageButton.Click();
+        }
+        public void Logout()
+        {
+
+            registrationDropdownMenu.Click();
+            logoutButton.Click();
+        }
+        public bool SuccessfulyLogout()
+        {
+            return successfulyLogout.Displayed;
+        }
+        //public MainPage GoToMainPage()
+        //{
+        //    GoToMain();
+        //    VerifyWebElements();
+        //    return this;
+        //}
+        protected override void VerifyWebElements()
+        {
+            IWebElement temp = Logo;
+            temp = registrationDropdownMenu;
+        }
+    }
+}
