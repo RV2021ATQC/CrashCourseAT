ec858791bede9bea74e716083e365552aff15f6a
SvitlanaLN <sv_lugan@ukr.net>
2021-05-09 23:35:35 +0300

    3_5 and OOP_1_1. Circle while + my first class Counter
    3_5 and OOP_1_1
    Circle while + my first class Counter

---
+++ Projects/SvitlanaLN/3_5_reverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class reverse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Input number (int value): ");
+            string inputString = Console.ReadLine();
+            int number;
+            while (!Int32.TryParse(inputString, out number))
+            {
+                Console.WriteLine(@"Could not convert '{0}' to an int. Input number (int value) again: ", inputString);
+                inputString = Console.ReadLine();
+            }
+            Console.WriteLine("Your number is: " + number);
+            Console.WriteLine();
+
+            int result =0;
+
+            while (number > 0)
+            {
+                result = result*10+number % 10;
+                Console.WriteLine($"Step1. Result is {result}");
+                number = number/10;
+                Console.WriteLine($"Step2. Number is {number}");
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Reversed number is {result}");
+            Console.ReadKey();
+        }
+    }
+
