7fd722284b7d49417c90cac5242670f6bc093f2e
SvitlanaLN <sv_lugan@ukr.net>
2021-08-24 20:57:19 +0300

    Selenium v2
    Selenium v2

--- Projects/SvitlanaLN/Selenium/Test_Selenium.cs
+++ Projects/SvitlanaLN/Selenium/Test_Selenium.cs
@@ -9,29 +9,30 @@ using System.Collections.Generic;
 
 namespace Selenium
 {
-    [TestFixture]
-    public class Test_Selenium
+    [TestFixture(typeof(ChromeDriver))]
+    [TestFixture(typeof(FirefoxDriver))]
+    public class Test_Selenium<TWebDriver> where TWebDriver : IWebDriver, new()
     {
-        private IWebDriver driver;
-        public enum BrowserType { Chrome, FireFox }
+        [ThreadStatic]
+        private IWebDriver driver;  
+        [SetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new TWebDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1000);
+        }
 
         [TearDown]
         public void AfterAllMethods()
         {
             driver.Quit();
-
         }
+
         [Test]
-        [Parallelizable]
-        [TestCase("sv_lugan@ukr.net", "!Qwerty123", BrowserType.Chrome)]      // must be true   
-        [TestCase("sv_lugan@ukr.net", "Wrong_password", BrowserType.Chrome)]  // must be false
-        [TestCase("sv_lugan@ukr.net", "!Qwerty123", BrowserType.FireFox)]      // must be true   
-        [TestCase("sv_lugan@ukr.net", "Wrong_password", BrowserType.FireFox)]  // must be false
-        public void Test_method(string usrn, string passw, BrowserType brow)
+        [TestCase("sv_lugan@ukr.net", "!Qwerty123")]      // must be true   
+        [TestCase("sv_lugan@ukr.net", "Wrong_password")]  // must be false
+        public void Test_method(string usrn, string passw)
         {
-            if (brow == BrowserType.Chrome) driver = new ChromeDriver();
-            else driver = new FirefoxDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             Page_Selenium obj = new Page_Selenium (driver);
             obj.Login(usrn, passw);
             Assert.AreEqual("Здравствуйте, Qwerty Qwerty", obj.Mes);
